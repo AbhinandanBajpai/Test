4449f3f62f606381df8fa11da8d064993d5bf74b
abhib <abhib@LAPTOP-T4RPB7RG>
2020-11-20 11:10:16 +0530

    2nd update

--- spring-data-jpa-mapping/src/main/java/com/javap/jpa/controller/OrderController.java
+++ spring-data-jpa-mapping/src/main/java/com/javap/jpa/controller/OrderController.java
@@ -4,14 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javap.jpa.dto.OrderRequest;
 import com.javap.jpa.entity.Customer;
+import com.javap.jpa.exception.ResourceNotFoundException;
 import com.javap.jpa.repository.CustomerRepository;
 import com.javap.jpa.repository.ProductRepository;
 
@@ -25,9 +29,11 @@ public class OrderController {
 	private ProductRepository productRepository;
 	
 	@PostMapping("/placeorder")
-	public Customer createOrder(@RequestBody OrderRequest orderRequest) {
+	public Customer createOrder(@RequestBody Customer customer) {
 		
-		return customerRepository.save(orderRequest.getCustomer());
+		System.out.println(customer);
+		
+		return customerRepository.save(customer);
 	}
 	
 	@GetMapping("/findAllOrders")
@@ -37,9 +43,35 @@ public class OrderController {
 		 List<Customer> findingAll = customerRepository.findingAll();
 		System.out.println(findingAll);
 		return customerRepository.findAll();
+	}	
+	
+	//finding By Id
+	
+	@GetMapping("findAllOrders/{id}")
+	public Customer findingById(@PathVariable int id) {
+		return customerRepository.findingById(id);
 	}
 	
-
+	//Updating the data
 	
+	@PutMapping("/update/{id}")
+	public Customer update(@PathVariable int id , @RequestBody Customer customer ) {
+
+	 return	customerRepository.findById(id).map(cust -> {
+			cust.setName(customer.getName());
+			cust.setEmail(customer.getEmail());
+			cust.setGender(customer.getGender());
+			cust.setProducts(customer.getProducts());
+			return customerRepository.save(cust);	
+		}).orElseThrow(()-> new ResourceNotFoundException("id"+id+"not found"));
+	}
+//	
+	//deleting the data
+	@DeleteMapping("/delete/{id}")
+	public Customer deletingById(@PathVariable int id) {
+		
+		 return customerRepository.deleteByID(id);
+		
+	}
 
 }
--- spring-data-jpa-mapping/src/main/java/com/javap/jpa/repository/CustomerRepository.java
+++ spring-data-jpa-mapping/src/main/java/com/javap/jpa/repository/CustomerRepository.java
@@ -4,14 +4,21 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.javap.jpa.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 
-	@Query(
-			  value = "SELECT * FROM customer u", 
+	@Query( value = "SELECT * FROM customer u", 
 			  nativeQuery = true)
 	List<Customer> findingAll();
+	
+	@Query(value= "SELECT * FROM customer u WHERE u.id = :id", nativeQuery = true)
+	Customer findingById(@Param("id") Integer id);
+	
+	@Query(value="DELETE FROM customer u WHERE u.id = :id", nativeQuery = true)
+	Customer deleteByID(@Param("id") Integer id);
+	
 
 }
